e79399c52cc48686c4ee91b31ba55ec4b03ceea7
popiette <pier.jacquot@gmail.com>
2019-03-30 15:01:02 +0100

    function collisions.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ var screenSize = 400;
 var screenPosTop = 100;
 var screenPosLeft = 100;
 
-var ball = new Rectangle(400, 175, 10, 10, 1, 1);
+var ball = new Rectangle(300, 300, 10, 10, 0, 1);
 var racket1 = new Rectangle(280, 150, 40, 10, 0, 0);
 var racket2 = new Rectangle(280, 450, 40, 10, 0, 0);
 
@@ -31,16 +31,23 @@ window.onload = function (){
 }
 
 function moveBall(){
-	if(ball.posTop - screenPosTop + ball.height >= screenSize 
-		|| ball.posTop - screenPosTop <= 0){
-		ball.moveTop = - ball.moveTop;
-	}
-	
-	if(ball.posLeft - screenPosLeft + ball.width >= screenSize
-		|| ball.posLeft - screenPosLeft <= 0){
-		ball.moveLeft = - ball.moveLeft;
-	}
+	collisions();
 	ball.move();
 	window.document.getElementById("ball").style.top = ball.posTop + "px";
 	window.document.getElementById("ball").style.left = ball.posLeft + "px";
+}
+
+function collisions(){
+	var ballTop = ball.posTop - screenPosTop;
+	var ballLeft = ball.posLeft - screenPosLeft;
+	
+	if(ballTop + ball.height >= screenSize 
+		|| ballTop <= 0){
+		ball.moveTop = - ball.moveTop;
+	}
+	
+	if(ballLeft + ball.width >= screenSize
+		|| ballLeft <= 0){
+		ball.moveLeft = - ball.moveLeft;
+	}
 }
