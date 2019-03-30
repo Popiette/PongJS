3b26d6b95f93aec1c1d0c0157e89d6fe3dff6a43
popiette <pier.jacquot@gmail.com>
2019-03-30 13:21:18 +0100

    Class Ball.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+class Ball{
+
+	constructor(posTop, posLeft, size, moveTop, moveLeft){
+		this.posTop = posTop;
+		this.posLeft = posLeft;
+		this.size = size;
+		this.moveTop = moveTop;
+		this.moveLeft = moveLeft;
+	}
+
+	move(){
+		console.log("moveTop : " + this.moveTop + " et moveLeft : " + this.moveLeft);
+		this.posTop += this.moveTop;
+		this.posLeft += this.moveLeft;
+	}
+}
+
 var screenSize = 400;
 
 var screenPosTop = 100;
 var screenPosLeft = 100;
 
-var posBallTop = 400;
-var posBallLeft = 175;
-
-var ballMoveTop = 1;
-var ballMoveLeft = 1;
+var ball = new Ball(400, 175, 10, 1, 1);
 
 window.onload = function (){
 	console.log("Hello !");
@@ -15,17 +28,16 @@ window.onload = function (){
 }
 
 function moveBall(){
-	if(posBallTop - screenPosTop + 10 >= screenSize 
-		|| posBallTop - screenPosTop <= 0){
-		ballMoveTop = -ballMoveTop;
+	if(ball.posTop - screenPosTop + ball.size >= screenSize 
+		|| ball.posTop - screenPosTop <= 0){
+		ball.moveTop = - ball.moveTop;
 	}
 	
-	if(posBallLeft - screenPosLeft + 10 >= screenSize
-		|| posBallLeft - screenPosLeft <= 0){
-		ballMoveLeft = -ballMoveLeft;
+	if(ball.posLeft - screenPosLeft + ball.size >= screenSize
+		|| ball.posLeft - screenPosLeft <= 0){
+		ball.moveLeft = - ball.moveLeft;
 	}
-	posBallTop += ballMoveTop;
-	posBallLeft += ballMoveLeft;
-	window.document.getElementById("ball").style.top = posBallTop + "px";
-	window.document.getElementById("ball").style.left = posBallLeft + "px";
+	ball.move();
+	window.document.getElementById("ball").style.top = ball.posTop + "px";
+	window.document.getElementById("ball").style.left = ball.posLeft + "px";
 }
