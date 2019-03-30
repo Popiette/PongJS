c19197da75843cf2d5257c39201e4893cc1bcffa
popiette <pier.jacquot@gmail.com>
2019-03-30 12:48:56 +0100

    Movement of ball.

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+var screenSize = 400;
+
+var screenPosTop = 100;
+var screenPosLeft = 100;
+
+var posBallTop = 400;
+var posBallLeft = 175;
+
+var ballMoveTop = 1;
+var ballMoveLeft = 1;
+
 window.onload = function (){
 	console.log("Hello !");
+	window.setInterval(moveBall, 10);
+}
+
+function moveBall(){
+	if(posBallTop - screenPosTop + 10 >= screenSize 
+		|| posBallTop - screenPosTop <= 0){
+		ballMoveTop = -ballMoveTop;
+	}
+	
+	if(posBallLeft - screenPosLeft + 10 >= screenSize
+		|| posBallLeft - screenPosLeft <= 0){
+		ballMoveLeft = -ballMoveLeft;
+	}
+	posBallTop += ballMoveTop;
+	posBallLeft += ballMoveLeft;
+	window.document.getElementById("ball").style.top = posBallTop + "px";
+	window.document.getElementById("ball").style.left = posBallLeft + "px";
 }
